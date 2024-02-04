d155371f37cf80f9ec4526cc40a953372fd9a151
Andrei Subach <ash@ursae.local>
2024-02-04 12:26:05 +0100

    prettier configuration

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,21 +1,29 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup {
-            sources = {
-                -- null_ls.builtins.formatting.templ,
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettier.with {
-                    filetypes = { "xsl", "html", "markdown", "css", "templ" }
-                },
-                null_ls.builtins.diagnostics.eslint_d,
-                null_ls.builtins.diagnostics.staticcheck,
-                null_ls.builtins.formatting.goimports,
-                null_ls.builtins.formatting.goimports_reviser,
-                null_ls.builtins.formatting.gofumpt,
-            }
-        }
-        vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "NoneLS: code formatting" })
-    end,
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier.with({
+					filetypes = {
+						"javascript",
+						"json",
+						"xsl",
+						"html",
+						"typescript",
+						"markdown",
+						"css",
+					},
+					extra_args = { "--no-semi" },
+				}),
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.diagnostics.staticcheck,
+				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.goimports_reviser,
+				null_ls.builtins.formatting.gofumpt,
+			},
+		})
+		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "NoneLS: code formatting" })
+	end,
 }
