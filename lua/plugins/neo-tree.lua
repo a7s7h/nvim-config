8f21114528d841439328000425befb8dad4777d8
Andrei Subach <ash@ursae.local>
2024-02-04 22:19:43 +0100

    added some neo-tree options

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,19 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle=true<CR>", { desc = "Neotree: show" })
-    end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+        require("neo-tree").setup({
+            window = {
+                width = 30,
+                side = "left",
+                auto_resize = true,
+            },
+        })
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle=true<CR>", { desc = "Neotree: show" })
+	end,
 }
