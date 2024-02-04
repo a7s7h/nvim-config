2a6ec5074109f96caba616f560b42735ddc68def
Andrei Subach <ash@ursae.local>
2024-02-04 21:51:02 +0100

    cleaning

--- lua/plugins/folding.lua
+++ lua/plugins/folding.lua
@@ -1,19 +1,17 @@
 return {
-    "kevinhwang91/nvim-ufo",
-    event = "BufEnter",
-    dependencies = {
-        "kevinhwang91/promise-async",
-    },
-    config = function()
-        local ufo = require("ufo")
-        ufo.setup({
-            {
-                provider_selector = function(_, _, _)
-                    return { "treesitter", "indent" }
-                end,
-            },
-        })
-        vim.keymap.set("n", "<leader>uo", ufo.openAllFolds, { desc = "UFO: open all folds" })
-        vim.keymap.set("n", "<leader>uc", ufo.closeAllFolds, { desc = "UFO: close all folds" })
-    end,
+	"kevinhwang91/nvim-ufo",
+	event = "BufEnter",
+	dependencies = {
+		"kevinhwang91/promise-async",
+	},
+	config = function()
+		local ufo = require("ufo")
+		ufo.setup({
+			{
+				provider_selector = function(_, _, _)
+					return { "treesitter", "indent" }
+				end,
+			},
+		})
+	end,
 }
