616e2b3822342a055c652a842328e804b67a36d2
Andrei Subach <ash@ursae.local>
2024-02-04 23:40:40 +0100

    some options optimized

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,13 +4,22 @@ vim.api.nvim_command("set relativenumber")
 vim.api.nvim_command("set nowrap")
 vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
-vim.opt.clipboard = "unnamed,unnamedplus"
+vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
 vim.opt.foldcolumn = "0"
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
 vim.opt.shortmess:append("sI")
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 999
+vim.opt.virtualedit = "block"
+vim.opt.inccommand = "nosplit" -- split is annoying
+vim.opt.ignorecase = true
 -- disable built-in providers
 for _, provider in ipairs({ "node", "perl", "python3", "ruby" }) do
 	vim.g["loaded_" .. provider .. "_provider"] = 0
@@ -32,11 +41,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank({ timeout = 200 })
 	end,
 })
--- set tabulation to 4 spaces
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
 -- various keymaps
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down centered" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up centered" })
