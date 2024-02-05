96c650b596a0d447247bd1f3bd27c16cfcf5a66f
Andrei Subach <ash@ursae.local>
2024-02-05 20:12:10 +0100

    reorganization

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,5 +1,17 @@
 return {
 	{
+		"windwp/nvim-autopairs",
+		opts = {
+			fast_wrap = {},
+			disable_filetype = { "TelescopePrompt", "vim" },
+		},
+		config = function(_, opts)
+			require("nvim-autopairs").setup(opts)
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
+	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
