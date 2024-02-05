8c75e0685645de9c266ce0f32d8a88bd0e7976ce
Andrei Subach <ash@ursae.local>
2024-02-05 09:20:53 +0100

    toggle transparency

--- lua/plugins/transparency.lua
+++ lua/plugins/transparency.lua
@@ -1,3 +1,11 @@
 return {
-    "tribela/vim-transparent"
+    "tribela/vim-transparent",
+    config = function()
+        vim.keymap.set(
+            "n",
+            "<leader>tb",
+            ":TransparentToggle<CR>",
+            { desc = "Toggle transparency", noremap = true, silent = true }
+        )
+    end,
 }
