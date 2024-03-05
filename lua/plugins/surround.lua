182dc744e44ac556a7b625bd31f92e0c8d299f0b
Andrei Subach <ash@ursae.local>
2024-03-05 19:05:21 +0100

    minor changes

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,9 +1,3 @@
 return {
-	-- "kylechui/nvim-surround",
-	-- version = "*",
-	-- event = "VeryLazy",
-	-- config = function()
-	-- 	require("nvim-surround").setup({})
-	-- end,
-    "tpope/vim-surround",
+	"tpope/vim-surround",
 }
