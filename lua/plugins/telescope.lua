b4d8a3e0cfce02f553774931749cdf5f82b9681b
a7s7h <ash@ursae.local>
2024-05-19 23:26:08 +0200

    removed bufferline

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,7 @@ return {
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: find files" })
 			vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Telescope: quick fix" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope: buffers" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: help tags" })
 			vim.keymap.set("n", "<leader>gf", builtin.help_tags, { desc = "Telescope: git files" })
 		end,
