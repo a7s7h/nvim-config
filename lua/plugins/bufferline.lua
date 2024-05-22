a7a197cf0bf6f32df22c24523f1ce2aab28622f7
a7s7h <ash@ursae.local>
2024-05-22 10:58:03 +0200

    added bufferline back

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,29 +1,29 @@
 return {
-	-- "akinsho/bufferline.nvim",
-	-- priority = 1000,
-	-- version = "*",
-	-- dependencies = "nvim-tree/nvim-web-devicons",
-	-- config = function()
-	-- 	local bufferline = require("bufferline")
-	-- 	bufferline.setup({
-	-- 		options = {
-	-- 			show_close_icon = true,
-	-- 			max_prefix_length = 8,
-	-- 			themable = true,
-	-- 			style_preset = bufferline.style_preset.no_bold,
-	-- 			offsets = {
-	-- 				{
-	-- 					filetype = "neo-tree",
-	-- 					separator = true,
-	-- 				},
-	-- 			},
-	-- 			numbers = "none",
-	-- 			color_icons = true,
-	-- 			show_buffer_icons = true,
-	-- 			show_buffer_close_icons = true,
-	-- 			separator_style = "thin",
-	-- 			always_show_bufferline = true,
-	-- 		},
-	-- 	})
-	-- end,
+	"akinsho/bufferline.nvim",
+	priority = 1000,
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local bufferline = require("bufferline")
+		bufferline.setup({
+			options = {
+				show_close_icon = true,
+				max_prefix_length = 8,
+				themable = true,
+				style_preset = bufferline.style_preset.no_bold,
+				offsets = {
+					{
+						filetype = "neo-tree",
+						separator = true,
+					},
+				},
+				numbers = "none",
+				color_icons = true,
+				show_buffer_icons = true,
+				show_buffer_close_icons = true,
+				separator_style = "thin",
+				always_show_bufferline = true,
+			},
+		})
+	end,
 }
