0d941be7d24370c970de87d184cb1c301d058a82
Andrew Subach <andrew.subach@gmail.com>
2025-04-28 20:42:17 +0200

    minor changes

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,27 +1,27 @@
 return {
-  "stevearc/conform.nvim",
-  config = function()
-    local conform = require("conform")
-    conform.setup({
-      formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        xml = { "xmllint" },
-        xslt = { "xmllint" },
-        lua = { "stylua" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
-      },
-    })
-  end,
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				xml = { "xmllint" },
+				xslt = { "xmllint" },
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+	end,
 }
